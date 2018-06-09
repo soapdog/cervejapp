1f029f1c5a7eea44290a5055fd4047b4d996096f
andre alves garzia <andre@andregarzia.com>
2018-06-09 00:35:06 -0300

    tá pronto!

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,31 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Round
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { umaCerveja : Float
+    , quantasCervejas : Float
+    , quantosPorcento : Float
+    , quantasPessoas : Float
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { umaCerveja = 0
+      , quantasCervejas = 0
+      , quantosPorcento = 10
+      , quantasPessoas = 1
+      }
+    , Cmd.none
+    )
 
 
 
@@ -21,23 +33,71 @@ init =
 
 
 type Msg
-    = NoOp
+    = MudaPrecoDaCerveja String
+    | MudaQuantasCervejas String
+    | MudaPorcentagem String
+    | MudaQuantasPessoas String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        MudaPrecoDaCerveja novoPreco ->
+            ( { model | umaCerveja = Result.withDefault 0 (String.toFloat novoPreco) }, Cmd.none )
+
+        MudaQuantasCervejas quantidade ->
+            ( { model | quantasCervejas = Result.withDefault 0 (String.toFloat quantidade) }, Cmd.none )
+
+        MudaPorcentagem porcentagem ->
+            ( { model | quantosPorcento = Result.withDefault 0 (String.toFloat porcentagem) }, Cmd.none )
+
+        MudaQuantasPessoas pessoas ->
+            ( { model | quantasPessoas = Result.withDefault 0 (String.toFloat pessoas) }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+criaCampo : String -> String -> (String -> Msg) -> Html Msg
+criaCampo nome legenda evento =
+    div []
+        [ label [ for nome ] [ text legenda ]
+        , input
+            [ type_ "num"
+            , id nome
+            , onInput evento
+            , size 4
+            ]
+            []
+        ]
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    let
+        quantoBebemos =
+            model.umaCerveja * model.quantasCervejas
+
+        custoTotal =
+            quantoBebemos + (quantoBebemos * (model.quantosPorcento / 100))
+
+        custoPorPessoa =
+            custoTotal / model.quantasPessoas |> Round.round 2
+    in
+    div
+        [ style
+            [ ( "font-size", "2.2rem" )
+            , ( "display", "flex" )
+            , ( "flex-direction", "column" )
+            ]
+        ]
+        [ h1 [] [ text "🍺 Cervejapp 🍺" ]
+        , criaCampo "umaCerveja" "💲🍺 x " MudaPrecoDaCerveja
+        , criaCampo "quantasCervejas" "🍻\x1F914 x " MudaQuantasCervejas
+        , criaCampo "quantasPessoas" "👥🍺x " MudaQuantasPessoas
+        , criaCampo "quantasCervejas" "\x1F935\x1F911 x " MudaPorcentagem
+        , h3 [] [ text ("💸😿 = R$ " ++ custoPorPessoa) ]
         ]
 
 
